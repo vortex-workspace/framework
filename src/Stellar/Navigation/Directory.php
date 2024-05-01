b7e57ceb776ee04e66469986098f3549c3f53dc6
mex3890 <abcv.tenorio@gmail.com>
2024-05-01 01:02:39 -0300

    Finish Directory scan and delete recursive

--- src/Stellar/Navigation/Directory.php
+++ src/Stellar/Navigation/Directory.php
@@ -2,15 +2,16 @@
 
 namespace Stellar\Navigation;
 
-use Stellar\Helpers\StrTool;
-use Stellar\Navigation\Directory\Enums\ScanSortMode;
 use Stellar\Navigation\Directory\Exceptions\FailedOnCreateDirectory;
 use Stellar\Navigation\Directory\Exceptions\FailedOnDeleteDirectory;
-use Stellar\Navigation\Directory\Exceptions\FailedOnScanDirectory;
+use Stellar\Navigation\Directory\Traits\Scan;
+use Stellar\Navigation\File\Exceptions\FailedOnDeleteFile;
 use Stellar\Navigation\Path\Exceptions\PathNotFound;
 
 class Directory extends Path
 {
+    use Scan;
+
     /**
      * @param string $path
      * @param bool $is_real_path
@@ -40,15 +41,37 @@ class Directory extends Path
 
     /**
      * @param string $directory_path
+     * @param bool $force
      * @param bool $is_real_path
      * @return bool
      * @throws FailedOnDeleteDirectory
+     * @throws FailedOnDeleteFile
      * @throws PathNotFound
      */
-    public static function delete(string $directory_path, bool $is_real_path = false): bool
+    public static function delete(string $directory_path, bool $force = false, bool $is_real_path = false): bool
     {
         if (!$is_real_path) {
             $directory_path = static::realPath($directory_path);
+        }
+
+        if (!empty($scan = self::scan(
+            $directory_path,
+            is_real_path: true,
+            key_as_path: true,
+            return_full_path: true,
+            exclude_parents: true
+        ))) {
+            if ($force === false) {
+                throw new FailedOnDeleteDirectory($directory_path);
+            }
+
+            foreach ($scan as $single_element) {
+                if (Path::isDirectory($single_element)) {
+                    self::delete($single_element, true, true);
+                } else {
+                    File::delete($single_element, true);
+                }
+            }
         }
 
         if (rmdir($directory_path) === false) {
@@ -56,79 +79,5 @@ class Directory extends Path
         }
 
         return true;
-    }
-
-    /**
-     * @param string $directory_path
-     * @param ScanSortMode $scanSortMode
-     * @param bool $is_real_path
-     * @param bool $key_as_path
-     * @param bool $exclude_parents
-     * @return array
-     * @throws FailedOnScanDirectory
-     * @throws PathNotFound
-     */
-    public static function scan(
-        string       $directory_path,
-        ScanSortMode $scanSortMode = ScanSortMode::None,
-        bool         $is_real_path = false,
-        bool         $key_as_path = false,
-        bool         $exclude_parents = true
-    ): array
-    {
-        if (!$is_real_path) {
-            $directory_path = static::realPath($directory_path);
-        }
-
-        if (!($scan = scandir($directory_path, $scanSortMode->value))) {
-            throw new FailedOnScanDirectory($directory_path);
-        }
-
-        if ($exclude_parents) {
-            array_pop($scan);
-            array_pop($scan);
-        }
-
-        if ($key_as_path === true) {
-            $formated_scan = [];
-
-            foreach ($scan as $path) {
-                $full_path = self::realPath(StrTool::forceFinishWith($directory_path, '/') .
-                    StrTool::removeIfStartAndFinishWith($path, '/'));
-
-                $formated_scan[$full_path] = $full_path;
-            }
-
-            $scan = $formated_scan;
-        }
-
-        return $scan;
-    }
-
-    /**
-     * @param string $directory_path
-     * @param ScanSortMode $scanSortMode
-     * @param bool $is_real_path
-     * @return array
-     * @throws FailedOnScanDirectory
-     * @throws PathNotFound
-     */
-    public static function recursiveScan(
-        string       $directory_path,
-        ScanSortMode $scanSortMode = ScanSortMode::None,
-        bool         $is_real_path = false
-    ): array
-    {
-        $scan = self::scan($directory_path, $scanSortMode, $is_real_path, true);
-dump($scan);
-sleep(2);
-        foreach ($scan as $value) {
-            if (self::isDirectory($value, true)) {
-                dump('Recursive: ' . $value);
-                $scan[$value] = self::recursiveScan($value, $scanSortMode, true);
-            }
-        }
-
-        return $scan;
     }
 }
--- src/Stellar/Navigation/Directory/Exceptions/FailedOnDeleteDirectory.php
+++ src/Stellar/Navigation/Directory/Exceptions/FailedOnDeleteDirectory.php
@@ -11,7 +11,7 @@ class FailedOnDeleteDirectory extends Exception
     public function __construct(public readonly string $path)
     {
         parent::__construct(
-            "Fail on try delete directory, \"$this->path\".",
+            "Fail on try delete directory, check if directory is empty or set force mode, \"$this->path\".",
             ExceptionCode::CATCH_EXCEPTION,
             Level::Warning
         );
